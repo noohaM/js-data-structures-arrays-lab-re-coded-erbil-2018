20e515f7007eed4195f7fee349b6aca1e4d1c35e
nooha mohammed <noohamohammed00@gmail.com>
2018-09-05 17:58:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution here!
+const drivers=["Milo", "Otis", "Garfield"]
+function destructivelyAppendDriver(name){
+  return drivers.push(name)
+}
+
+function destructivelyPrependDriver(name){
+  return drivers.unshift(name)
+}
+
+function destructivelyPrependDriver(){
+  return drivers.pop(name)
+}
+
+function destructivelyPrependDriver(name){
+  return drivers.unshift(name)
+}
+function destructivelyPrependDriver(name){
+  return drivers.shift(name)
+}
