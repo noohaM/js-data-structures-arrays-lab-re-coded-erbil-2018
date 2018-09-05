807c1dbb07cec69ad6c891c917407de550184952
nooha mohammed <noohamohammed00@gmail.com>
2018-09-05 18:04:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your solution here!
-const drivers=["Milo", "Otis", "Garfield"]
+const drivers = ["Milo", "Otis", "Garfield"]
+
 function destructivelyAppendDriver(name){
   return drivers.push(name)
 }
@@ -8,10 +9,30 @@ function destructivelyPrependDriver(name){
   return drivers.unshift(name)
 }
 
-function destructivelyPrependDriver(){
+function destructivelyRemoveLastDriver(){
   return drivers.pop(name)
 }
 
 function destructivelyRemoveFirstDriver(){
   return drivers.shift(name)
+}
+
+function appendDriver(name){
+  apDriverArray = [...drivers,name]
+  return apDriverArray
+}
+
+function prependDriver(name){
+  prDriverArray = [name,...drivers]
+  return prDriverArray
+}
+
+function removeLastDriver(){
+  newArray = drivers.slice(0,drivers.length - 1)
+  return newArray
+}
+
+function removeFirstDriver(){
+  newArray = drivers.slice(1,drivers.length)
+  return newArray
 }
